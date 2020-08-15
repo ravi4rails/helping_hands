7edaf2ad2fb10e5e03193d74bb1cd837cc1a0fe6
Ravi Singh <ravionrails@gmail.com>
2020-08-16 01:34:49 +0530

    added default scope to story response

--- app/models/story_response.rb
+++ app/models/story_response.rb
@@ -4,8 +4,14 @@ class StoryResponse < ApplicationRecord
 
   validate :check_for_same_user, on: :create
 
+  default_scope -> { order(created_at: :desc) }
+
   def check_for_same_user
     same_id = user_id == story.user_id
     errors.add(:user, 'can not respond to own story') if same_id
+  end
+
+  def responder
+    user.name
   end
 end
