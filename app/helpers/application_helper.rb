1ed14d11efffc91f74537935314ff9a79fcba81f
Ravi Singh <ravionrails@gmail.com>
2020-08-16 17:13:57 +0530

    added partials for flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when :success then 'alert alert-success'
+      when :notice then 'alert alert-success'
+      when :alert then 'alert alert-danger'
+      when :error then 'alert alert-danger'
+      when :warning then 'alert alert-warning'
+    end
+  end
+
   def show_response_count(story_response)
     story = story_response.story
     story.story_responses.where(user_id: story_response.user.id).count
