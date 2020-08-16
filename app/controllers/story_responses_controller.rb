d58f638f06f4b7404fef2d0be800e2b3f39fee4b
Ravi Singh <ravionrails@gmail.com>
2020-08-16 17:34:34 +0530

    added error message if response rollback

--- app/controllers/story_responses_controller.rb
+++ app/controllers/story_responses_controller.rb
@@ -2,9 +2,13 @@ class StoryResponsesController < ApplicationController
   before_action :authenticate_user!, only: %i[create]
   before_action :set_story
   def create
-    @story_response = set_story.story_responses.create(responses_params)
+    @story_response = set_story.story_responses.new(responses_params)
     respond_to do |format|
-      format.html { redirect_to set_story, notice: 'Thank you for your response' }
+      if @story_response.save
+        format.html { redirect_to set_story, notice: 'Thank you for your response' }
+      else
+        format.html { redirect_to set_story, alert: 'You can not respond to your own story' }
+      end
     end
   end
 
