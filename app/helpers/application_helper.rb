c8e1f6b1f5b74c7a1a3d67c8acbec6e93379f780
Ravi Singh <ravionrails@gmail.com>
2020-08-16 16:20:10 +0530

    correct the order of display response

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def show_response_count(story_response)
+    story = story_response.story
+    story.story_responses.where(user_id: story_response.user.id).count
+  end
 end
